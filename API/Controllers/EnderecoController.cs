cd21988bee01dc1701877d89f6dc6e3eac7c9829
Arthur Guelle <arthurguelle@gmail.com>
2024-09-18 00:07:02 -0300

    Camada API

---
+++ API/Controllers/EnderecoController.cs
@@ -0,0 +1,50 @@
+﻿using Domains.Interfaces.Services;
+using Domains.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController : ControllerBase
+    {
+        private readonly IEnderecoService _service;
+
+        public EnderecoController(IEnderecoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [Route("list")]
+        public async Task <IActionResult> Add(List<EnderecoModel> model)
+        {
+            await _service.Add(model);
+            return Ok(new {Message = "Ceps Adicionado com Sucesso"});
+        }
+
+        [HttpGet]
+        [Route("ObterCepParaTratamento")]
+        public async Task<IActionResult> ObterCepParaTratamento(string robot0)
+        {
+            var cep = await _service.ObterCepParaTratamento(robot0);
+            return Ok(cep);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var ceps = await _service.List();
+            return Ok(ceps);
+        }
+
+        [HttpPut]
+        [Route("AtualizarDados")]
+        public async Task<IActionResult> AtualizarDados(EnderecoModel model)
+        {
+            await _service.AtualizarDados(model);
+            return Ok(new { Message = "Ceps Atualizados com Sucesso" });
+        }
+    }
+
+}
---
+++ API/IoC/DI.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services;
+using Domains.Interfaces.Repository;
+using Domains.Interfaces.Services;
+using Infraestructure;
+using Infraestructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.IoC
+{
+    public static class DI
+    {
+        public static void AddSdk(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddScoped<IEnderecoService, EnderecoService>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddDbContext<ProjectDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });   
+        }
+    }
+}
