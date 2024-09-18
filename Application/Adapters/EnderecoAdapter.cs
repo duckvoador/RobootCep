8daa4f9ca717a82c46766d26eff55312389b0423
Arthur Guelle <arthurguelle@gmail.com>
2024-09-17 22:49:12 -0300

    Camada Aplication

---
+++ Application/Adapters/EnderecoAdapter.cs
@@ -0,0 +1,42 @@
+﻿using Domains.Entidades;
+using Domains.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Adapters
+{
+    public class EnderecoAdapter
+    {
+        public static EnderecoModel Map(Endereco endereco) 
+        {
+            var model = new EnderecoModel();
+            model.Id = endereco.Id;
+            model.CEP = endereco.CEP;
+            model.Logadouro = endereco.Logadouro;
+            model.UF = endereco.UF;
+            return model;
+        }
+        public static Endereco Map(EnderecoModel endereco)
+        {
+            var model = new Endereco();
+            model.Id = endereco.Id;
+            model.CEP = endereco.CEP;
+            model.Logadouro = endereco.Logadouro;
+            model.UF = endereco.UF;
+            return model;
+        }
+        public static List<EnderecoModel> Map(List<Endereco> endereco) 
+        {
+            return endereco.Select(Map).ToList();
+        }
+        public static List<Endereco> Map(List<EnderecoModel> endereco)
+        {
+            return endereco.Select(Map).ToList();
+        }
+    }
+
+
+}
---
+++ Application/Services/EnderecoService.cs
@@ -0,0 +1,54 @@
+﻿using Application.Adapters;
+using Domains.Entidades;
+using Domains.Interfaces.Repository;
+using Domains.Interfaces.Services;
+using Domains.Models;
+
+namespace Application.Services
+{
+    public class EnderecoService : IEnderecoService
+    {
+        private readonly IEnderecoRepository _repository;
+
+        public EnderecoService(IEnderecoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Add(List<EnderecoModel> enderecos)
+        {
+            var domains = EnderecoAdapter.Map(enderecos);
+            await _repository.Add(domains);
+        }
+
+        public async Task AtualizarDados(EnderecoModel endereco)
+        {
+            var domain = await _repository.Get(endereco.Id);
+            if (domain == null)
+            {
+                throw new Exception("Cep não Localizado");
+            }
+            domain.Logadouro = endereco.Logadouro;
+            domain.Bairro = endereco.Bairro;
+            domain.Status = Domains.Enums.EnumStatus.Finalizado;
+
+            await _repository.AtualizarDados(domain);            
+        }
+
+        public async Task<List<EnderecoModel>> List()
+        {
+            var enderecos = await _repository.List();
+            return EnderecoAdapter.Map(enderecos);
+        }
+
+        public async Task<EnderecoModel> ObterCepParaTratamento(string robo0)
+        {
+            var domain = await _repository.ObterCepParaTratamento(robo0);
+            domain.Status = Domains.Enums.EnumStatus.EmAndamento;
+            domain.Robo = robo0;
+
+            _repository.AtualizarDados(domain);
+            return EnderecoAdapter.Map(domain);
+        }
+    }
+}
